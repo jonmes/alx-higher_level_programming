c9f6b70abf7efeb4f6b72469c4e55edd07695fef
jonmes <mesfinjohn@rocketmail.com>
2022-06-21 19:25:00 +0000

    commit

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class Square:
+    """Represents a square.
+    Private instance attribute: size."""
+
+    def __init__(self, size):
+        """Initializes the data."""
+        self.__size = size
