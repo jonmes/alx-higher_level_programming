30cfd0a4671f2bf881a766b3cf1ec974325d4f88
jonmes <mesfinjohn@rocketmail.com>
2022-08-11 15:02:07 -0700

    commits

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,3 @@
+-- write a script grant all privilege
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,6 @@
+-- Write a script create new users
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL 
+ON *.* 
+TO 'user_0d_1'@'localhost';
