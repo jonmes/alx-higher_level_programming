e80799e177a0d7a267e1663f2c5c7499c1c398cf
jonimes <jonimsfn@gmail.com>
2022-10-20 05:23:17 +0300

    api hot fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -10,7 +10,7 @@ function addToDict (url, name) {
   dict[url] = name;
 }
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
@@ -42,7 +42,7 @@ function helpRequest (arr, i) {
   });
 }
 
-request('http://swapi.co/api/films/' + process.argv[2], function (error, response, body) {
+request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (error, response, body) {
   if (error) {
     console.error(error);
   }
